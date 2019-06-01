821169168705f3b2638270d236ea72a32820be53
José Correia <josecorreia@Joses-MacBook-Pro.local>
2019-06-01 21:06:00 +0100

    new implementation

--- app/video_stream/__init__.py
+++ app/video_stream/__init__.py
@@ -1,9 +1,4 @@
 from flask import Blueprint
 bp = Blueprint('video_stream', __name__)
 
-# Setup Video -----------------------------------
-import cv2
-video_camera = cv2.VideoCapture(0) 
-
-
 from app.video_stream import routes
--- app/video_stream/routes.py
+++ app/video_stream/routes.py
@@ -1,24 +1,23 @@
 from app import logger
-from . import bp, video_camera
-from flask import flash, render_template, request, Response
+from . import bp
+from flask import render_template, Response
 
-import cv2
+from camera_pi import Camera
 
-@bp.route('/') 
-def index(): 
-   """Video streaming .""" 
-   return render_template('index.html') 
+@bp.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
-@bp.route('/video_feed') 
-def video_feed(): 
-   """Video streaming route. Put this in the src attribute of an img tag.""" 
-   return Response(generate_video(), 
-                   mimetype='multipart/x-mixed-replace; boundary=frame') 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-def generate_video(): 
-   """Video streaming generator function.""" 
-   while True: 
-       rval, frame = video_camera.read() 
-       cv2.imwrite('pic.jpg', frame) 
-       yield (b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n') 
+@bp.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
