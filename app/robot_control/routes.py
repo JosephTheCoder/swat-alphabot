e610f7c60cd2415d718e1c80ca8b517c95f6c963
José Correia <josecorreia@Joses-MacBook-Pro.local>
2019-06-01 17:34:38 +0100

    Update

--- app/robot_control/routes.py
+++ app/robot_control/routes.py
@@ -1,5 +1,5 @@
 from app import logger
-from flask import render_template
+from flask import render_template, Response
 from . import bp, Robot, GPIO, RIGHT_INFRARED, LEFT_INFRARED
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
@@ -11,16 +11,20 @@ def action(action):
    if action == "forward":
       if right_IR_status == 1 and left_IR_status == 1:
          Robot.forward()
+         return Response(status=200) 
    
    if action == "backward":
          Robot.backward()
+         return Response(status=200) 
 
    if action == "left":
       if left_IR_status == 1:
          Robot.left()
+         return Response(status=200) 
 
    if action == "right":
       if right_IR_status == 1:
          Robot.right()
+         return Response(status=200) 
 
-   return 200
+   return Response(status=400) 
