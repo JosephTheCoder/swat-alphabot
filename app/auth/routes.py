70271c0345ca608aab013bed2d2c1ff554b6de6b
JosephTheCoder <menesesjmmc@gmail.com>
2019-06-02 23:23:52 +0100

    Update

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -8,7 +8,7 @@ from flask_login import current_user, login_user, logout_user
 @bp.route('/')
 def index():
     if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
+        return render_template('login.html')
 
     return render_template('index.html')
 
