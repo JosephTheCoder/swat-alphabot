b829e2de88167108a6fa11ef3f19c04021be0efa
JosephTheCoder <menesesjmmc@gmail.com>
2019-06-05 21:03:01 +0100

    javascript on buttons

--- app/robot_control/routes.py
+++ app/robot_control/routes.py
@@ -5,30 +5,52 @@ from flask_login import login_required
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @login_required
-@bp.route("/move")
-def action():
+@bp.route("/move/forward")
+def forward():
    right_IR_status = GPIO.input(RIGHT_INFRARED)
    left_IR_status = GPIO.input(LEFT_INFRARED)
 
-   action = request.args.get('action')
-   logger.info(action)
+   if right_IR_status == 1 and left_IR_status == 1:
+      Robot.forward()
 
-   if action == "forward":
-      if right_IR_status == 1 and left_IR_status == 1:
-         Robot.forward()
+   return ('', 204)
 
-   if action == "backward":
-      Robot.backward()
 
-   if action == "left":
-      if left_IR_status == 1:
-         Robot.left()
+@login_required
+@bp.route("/move/left")
+def left():
+   right_IR_status = GPIO.input(RIGHT_INFRARED)
+   left_IR_status = GPIO.input(LEFT_INFRARED)
 
-   if action == "right":
-      if right_IR_status == 1:
-         Robot.right()
+   if left_IR_status == 1:
+      Robot.left()
 
-   if action == "stop":
-         Robot.stop()
+   return ('', 204)
 
-   return redirect(url_for('auth.index'))
+
+@login_required
+@bp.route("/move/right")
+def right():
+   right_IR_status = GPIO.input(RIGHT_INFRARED)
+   left_IR_status = GPIO.input(LEFT_INFRARED)
+
+   if right_IR_status == 1:
+      Robot.right()
+   
+   return ('', 204)
+
+
+@login_required
+@bp.route("/move/backward")
+def backward():
+   Robot.backward()
+
+   return ('', 204)
+
+
+@login_required
+@bp.route("/move/stop")
+def stop():
+   Robot.stop()
+
+   return ('', 204)
